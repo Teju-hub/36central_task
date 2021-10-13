d772e95af541a92344fd0820873bc91f05e565ad
liza <tejaswininayak2017@gmail.com>
2021-10-13 15:43:12 +0530

    36_central commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					<h4>Welcome to 36 Central Training and Development</h4>
+				</div>
+				<div class="card-body">
+					
+					<h5>Our Goals</h5>
+					<p>
+						We grown up in chhatisgarh, and our primary aim is to give our city, state best IT services and training by providing unique and innovative IT products and best learning experience. Our dream is to give our state a best IT Training Institute. 
+						Localite students can learn here, work here and thus it's our small approach to build IT culture here and new identity of Chhatisgarh in IT and technical world.
+					</p>
+					<p>
+						We convert any business idea into digital products that helps you grow ,also we give training with industrial standards. Our goal is to create bond of trust with our clients and trainee. we make people learn, we make your buisness grow with our product and services. 
+					</p>
+				</div>
+				<div class="card-footer">
+					© All Rights Reserved. 36 Central
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
